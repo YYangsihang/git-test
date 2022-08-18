68bd05f1b422193393c8b28aa320ee3d88c8156c
Yang <806452813@qq.com>
2022-08-18 15:27:29 +0800

    third

---
+++ src/main/java/com/yang/git.java
@@ -0,0 +1,10 @@
+package com.yang;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("jajjajaja");
+        System.out.println("wwww");
+        System.out.println("w9999");
+        System.out.println("w99");
+    }
+}
