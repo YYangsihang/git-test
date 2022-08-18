65cc990b8ce8ff6e2aa8795616b68f741ff0c476
Yang <806452813@qq.com>
2022-08-18 15:33:17 +0800

    hot

--- src/main/java/com/yang/git.java
+++ src/main/java/com/yang/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("jajjajaja");
         System.out.println("w99");
+        System.out.println("hot");
     }
 }
