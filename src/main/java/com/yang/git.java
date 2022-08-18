099458091088604d9c92dd072b80e78d1b580d7e
Yang <806452813@qq.com>
2022-08-18 15:28:07 +0800

    first

--- src/main/java/com/yang/git.java
+++ src/main/java/com/yang/git.java
@@ -3,8 +3,6 @@ package com.yang;
 public class git {
     public static void main(String[] args) {
         System.out.println("jajjajaja");
-        System.out.println("wwww");
-        System.out.println("w9999");
         System.out.println("w99");
     }
 }
